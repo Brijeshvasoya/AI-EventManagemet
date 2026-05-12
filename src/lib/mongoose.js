b3e9d88ef18cf1c5dc6becc2577cfa2e125c5444
Brijesh <brijeshvasoya@gmail.com>
2026-05-12 13:08:31 +0530

    add log

--- src/lib/mongoose.js
+++ src/lib/mongoose.js
@@ -23,6 +23,7 @@ async function connectToDatabase() {
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log('✅ Database connected successfully');
       return mongoose;
     });
   }
